d7f08985a175ac1df41159744fe833f7a221f493
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2026-04-24 03:22:58 +0530

    added regex string

---
+++ string/regex.js
@@ -0,0 +1,5 @@
+let str = "javascript";
+
+let count = str.match((/[aeiou]/gi)).length
+
+console.log(count,"898")
---
+++ string/string.js
@@ -0,0 +1,8 @@
+// string is immuitable: it means no change the value.
+// lenth property:str.length
+//slice:str.slice(1,4) it starts index start but last works 1 index below. ye -1 se bhi index leta h back side se.
+  
+
+let str = "anuj";
+str[2]="r"
+console.log(str.length);
