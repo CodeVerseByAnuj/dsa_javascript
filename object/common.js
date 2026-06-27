3afb1dcc3474131441cd0809dff68ed82372b62b
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2026-06-27 13:26:54 +0530

    object:added object common question

---
+++ object/common.js
@@ -0,0 +1,9 @@
+const user = {
+  name: "Anuj",
+  age: 25,
+  city: "Noida"
+};
+
+console.log(user.name)
+// another apporach show the age only allow type of the number
+console.log(user["age"])
---
+++ object/group.js
@@ -0,0 +1,9 @@
+const users = [
+  { name: "A", age: 20 },
+  { name: "B", age: 21 },
+  { name: "C", age: 20 }
+];
+
+for(let i of users){
+    console.log(user[i])
+}
