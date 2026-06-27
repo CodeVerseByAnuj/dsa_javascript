e0ff0f05df0db2151a2f69e13dae875aa89f72cc
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2026-06-27 15:34:52 +0530

    object:common

--- object/common.js
+++ object/common.js
@@ -4,6 +4,42 @@ const user = {
   city: "Noida"
 };
 
+// Time Complexity
+// Access → O(1)
+// Insert → O(1)
+// Delete → O(1)
 console.log(user.name)
 // another apporach show the age only allow type of the number
 console.log(user["age"])
+
+// Count Number of Properties
+const obj = {
+  a: 10,
+  b: 20,
+  c: 30
+};
+
+console.log(Object.keys(obj).length)
+
+//Iterate Over an Object
+const student = {
+  name: "Anuj",
+  age: 25,
+  city: "Delhi"
+};
+
+for (let stu in student) {
+  console.log(student[stu])
+}
+
+// Sum All Values
+const marks = {
+  math: 80,
+  english: 90,
+  science: 70
+};
+let sum = 0
+for (let mark in marks) {
+  sum = sum + marks[mark]
+}
+console.log(sum)
