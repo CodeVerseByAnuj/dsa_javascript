e5f77f2d7a918efdcbef4c6e28b4cd3e42a3f8dc
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2026-09-14 23:19:07 +0530

    refactor unique character function to find the index of the first non-repeating character

--- string/uniqueChar.js
+++ string/uniqueChar.js
@@ -1,13 +1,28 @@
-let s = "leetcode";
-let newchar = "";
+// let s = "eetcode";
+// let newchar = "";
 
-let ch = s.charAt(0);
+// for (let i = 0; i < s.length; i++) {
+//     let ch = s.charAt(i)
+//     if (!newchar.includes(ch)) {
+//         newchar = newchar + ch;
+//     }
+// }
 
-for (let i = 0; i < s.length; i++) {
-    let ch = s.charAt(i)
-    if (!newchar.includes(ch)) {
-        newchar = newchar + ch;
+function str(s) {
+    for (let i = 0; i < s.length; i++) {
+        let count = 0;
+        for (let j = i+1; j < s.length; j++) {
+
+            if (s[i] === s[j]) {
+                count++;
+            }
+        }
+
+        if (count === 1) {
+            return i;
+        }
     }
+    return -1;
 }
-console.log(newchar)
+console.log(str("leetcode"))
 
